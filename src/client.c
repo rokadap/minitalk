56e815a63ac5a22a1a684ce840a7ca4ec7258153
Reijiro Okada <rokada@c1r2s8.42tokyo.jp>
2025-06-04 19:56:29 +0900

    2

--- src/client.c
+++ src/client.c
@@ -6,50 +6,51 @@
 /*   By: rokada <rokada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 19:05:11 by rokada            #+#    #+#             */
-/*   Updated: 2025/06/03 22:54:01 by rokada           ###   ########.fr       */
+/*   Updated: 2025/06/04 19:19:39 by rokada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk.h"
 
-void    send_signal(int pid, char c)
+void	send_signal(int pid, char c)
 {
-    int	bit;
+	int	bit;
 
-    bit = 0;
-    while (bit < 8)
-    {
-        if ((c & (1 << bit)) != 0)
-        {
-            if (kill(pid, SIGUSR1) == -1)
-                error_handler("kill error", "SIGUSR1");
-        }
-        else
-        {
-            if (kill(pid, SIGUSR2) == -1)
-                error_handler("kill error", "SIGUSR2");
-        }
-        usleep(WAIT_TIME);
-        bit++;
-    }
+	bit = 0;
+	while (bit < 8)
+	{
+		if ((c & (1 << bit)) != 0)
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				error_handler("kill error", "SIGUSR1");
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error_handler("kill error", "SIGUSR2");
+		}
+		usleep(WAIT_TIME);
+		bit++;
+	}
 }
-int main(int argc, char **argv)
-{
-    int	pid;
-    int	i;
 
-    if (argc != 3)
-        error_handler("Invalid arguments", "Usage: ./client <PID> <message>");
-    if (!bool_atoi(argv[1], &pid))
-        error_handler("Invalid PID", "PID must be a positive integer");
-    if (pid <= 0 || pid > INT_MAX)
-        error_handler("Invalid PID", "PID must be a positive integer");
-    i = 0;
-    while (argv[2][i])
-    {
-        send_signal(pid, argv[2][i]);
-        i++;
-    }
-    send_signal(pid, '\n');
-    return (0);
+int	main(int argc, char **argv)
+{
+	int	pid;
+	int	i;
+
+	if (argc != 3)
+		error_handler("Invalid arguments", "Usage: ./client <PID> <message>");
+	if (!bool_atoi(argv[1], &pid))
+		error_handler("Invalid PID", "PID must be a positive integer");
+	if (pid <= 0 || pid > INT_MAX)
+		error_handler("Invalid PID", "PID must be a positive integer");
+	i = 0;
+	while (argv[2][i])
+	{
+		send_signal(pid, argv[2][i]);
+		i++;
+	}
+	send_signal(pid, '\n');
+	return (0);
 }
--- src/server_bonus.c
+++ src/server_bonus.c
@@ -6,51 +6,52 @@
 /*   By: rokada <rokada@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 19:05:04 by rokada            #+#    #+#             */
-/*   Updated: 2025/06/03 22:53:53 by rokada           ###   ########.fr       */
+/*   Updated: 2025/06/04 19:34:25 by rokada           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minitalk_bonus.h"
 
-void signal_handler(int signal,siginfo_t *info,void *context)
+void	signal_handler(int signal, siginfo_t *info, void *context)
 {
-    static int  bit;
-    static char c;
+	static int	bit;
+	static char	c;
 
-    if (signal == SIGUSR1)
-        c |= (1 << bit);
-    bit++;
-    if (bit == 8)
-    {
-        write(1, &c, 1);
-        bit = 0;
-        c = 0;
-        if (info->si_pid > 0)
-            kill(info->si_pid, signal);
-    }
-    (void)context;
+	if (signal == SIGUSR1)
+		c |= (1 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		write(1, &c, 1);
+		bit = 0;
+		c = 0;
+		if (info->si_pid > 0)
+			kill(info->si_pid, signal);
+	}
+	(void)context;
 }
-int main(int  argc, char **argv)
-{
-    struct sigaction sa;
 
-    (void)argv;
-    if (argc != 1)
-        error_handler("Invalid arguments", "Usage: ./server_bonus");
-    ft_printf("Server PID: %d\n", getpid());
-    sa.sa_flags = SA_SIGINFO;
-    sa.sa_sigaction = signal_handler;
-    sigemptyset(&sa.sa_mask);
-    if(sigaddset(&sa.sa_mask, SIGUSR1) == -1 ||
-       sigaddset(&sa.sa_mask, SIGUSR2) == -1)
-        error_handler("sigaddset error", NULL);
-    if (sigaction(SIGUSR1, &sa, NULL) == -1 ||
-        sigaction(SIGUSR2, &sa, NULL) == -1)
-        error_handler("sigaction error", NULL);
-    while (1)
-    {
-        pause();
-        usleep(WAIT_TIME);
-    }
-    return (0);
+int	main(int argc, char **argv)
+{
+	struct sigaction	sa;
+
+	(void)argv;
+	if (argc != 1)
+		error_handler("Invalid arguments", "Usage: ./server_bonus");
+	ft_printf("Server PID: %d\n", getpid());
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	if (sigaddset(&sa.sa_mask, SIGUSR1) == -1 ||
+		sigaddset(&sa.sa_mask, SIGUSR2) == -1)
+		error_handler("sigaddset error", NULL);
+	if (sigaction(SIGUSR1, &sa, NULL) == -1 ||
+		sigaction(SIGUSR2, &sa, NULL) == -1)
+		error_handler("sigaction error", NULL);
+	while (1)
+	{
+		pause();
+		usleep(WAIT_TIME);
+	}
+	return (0);
 }
